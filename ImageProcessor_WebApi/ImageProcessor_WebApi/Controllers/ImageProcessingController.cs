855c771ab89987a43ea383a9c2f744004ff0c65e
sabotamas0 <sabotamas0@gmail.com>
2025-07-24 18:57:38 +0200

    feature/WebApi_BusinessLogic: Added Separate project for validation, services, Implemented rest api, communication between layers

--- ImageProcessor_WebApi/ImageProcessor_WebApi/Controllers/ImageProcessingController.cs
+++ ImageProcessor_WebApi/ImageProcessor_WebApi/Controllers/ImageProcessingController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageProcessor_Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Threading;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using FluentValidation;
+
 
 namespace ImageProcessor_WebApi.Controllers
 {
-    public class ImageProcessingController : Controller
+    [Route("api/imageprocessing")]
+    [ApiController]
+    public class ImageProcessingController : ControllerBase
     {
-        public IActionResult Index()
+        private IImageProcessingService _service;
+        public ImageProcessingController(IImageProcessingService service) 
         {
-            return View();
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("process_image")]
+        public async Task<IActionResult> ProcessImage(IFormFile file, CancellationToken token)
+        {
+            try
+            {
+                using (var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(token))
+                {
+                    var response = await _service.ImageProcessingFormFile(file, linkedCts.Token);
+
+                    return File(response.Data, $"image/{response.ContentType}");
+                }
+            }
+            catch (OperationCanceledException e)
+            {
+                return Ok("Operation canceled while processing the image.");
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest("During the image processing there were some Validation Errors: " + string.Join(" | ", ex.Errors.Select(x => x.ErrorMessage)));
+            }
+
         }
     }
 }
