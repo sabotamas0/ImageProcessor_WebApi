481716d06c982d0a688746626b1a993b0cfe3a00
sabotamas0 <sabotamas0@gmail.com>
2025-07-24 20:49:54 +0200

    feature/WebApi_BusinessLogic: Prepare Api for larger files

--- ImageProcessor_WebApi/Program.cs
+++ ImageProcessor_WebApi/Program.cs
@@ -1,5 +1,6 @@
 
 using ImageProcessor_Application;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace ImageProcessor_WebApi
 {
@@ -10,7 +11,23 @@ namespace ImageProcessor_WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.Limits.MaxRequestBodySize = 524288000; // 500 MB
+            });
+            builder.Services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = 524288000; // 500 MB
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -25,8 +42,6 @@ namespace ImageProcessor_WebApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
-            
 
             app.UseAuthorization();
 
